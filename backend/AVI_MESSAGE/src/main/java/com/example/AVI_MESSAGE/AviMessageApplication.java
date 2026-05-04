4658452901400694ffec1c56860a54512c8a1a1a
AmanJangir07 <amanjangir200@gmail.com>
2026-05-04 13:59:20 +0530

    Add backend project AVI_MESSAGE

---
+++ backend/AVI_MESSAGE/src/main/java/com/example/AVI_MESSAGE/AviMessageApplication.java
@@ -0,0 +1,13 @@
+package com.example.AVI_MESSAGE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AviMessageApplication.class, args);
+	}
+
+}
---
+++ backend/AVI_MESSAGE/src/test/java/com/example/AVI_MESSAGE/AviMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AVI_MESSAGE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
